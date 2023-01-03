a531c625ca1960df6550b33f234d6f583b707b3f
albmonmar3 <albmonmar3@alum.us.es>
2023-01-03 14:43:12 +0100

    Título: #3 creadas vista que muestra imágenes de prueba Cuerpo: Se ha implementado una vista que muestra al usuario imágenes de prueba, en las que puede hacer click para que le redirijan a la vista de la predicción.

--- elevate/lynx/urls.py
+++ elevate/lynx/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
 
     # - Homepage
-
+    path('images/', views.images_view, name='images'),
+    path('images/<int:image_id>/', views.image_detail, name='image_detail'),
     path('', views.home, name=""),
 
 
--- elevate/lynx/views.py
+++ elevate/lynx/views.py
@@ -1,7 +1,71 @@
 from django.shortcuts import render
+from numpy import loadtxt
+import pandas as pd
+from PIL import Image
+from fastcore.all import *
+from torch.utils.data import Dataset ,DataLoader
+import torchvision.transforms as transforms
+from tqdm.notebook import tqdm
+import torch
+from torch import nn
+import torchmetrics as metrics
+import pytorch_lightning as pl
+
+
 
 # - Homepage
 
 def home(request):
 
     return render(request, 'lynx/index.html')
+
+
+from django.shortcuts import render
+
+def images_view(request):
+    # Aquí puedes obtener las imágenes que quieres mostrar en la página
+    # desde la base de datos o cualquier otro lugar.
+    # En este ejemplo, suponemos que tienes una lista de diccionarios
+    # con información sobre las imágenes.
+    images = [
+        {
+            'url': '/static/1163.jpg',
+            'name': 'Imagen 1',
+            'link': '/images/1',
+        },
+        {
+            'url': '/static/1535.jpg',
+            'name': 'Imagen 2',
+            'link': '/images/2',
+        },
+        # ...
+    ]
+
+    return render(request, 'images.html', {'images': images})
+
+
+
+# views.py
+
+def image_detail(request, image_id):
+
+    images = [
+        {
+            'url': '/static/1163.jpg',
+            'name': 'Imagen 1',
+            'link': '/images/1',
+        },
+        {
+            'url': '/static/1535.jpg',
+            'name': 'Imagen 2',
+            'link': '/images/2',
+        },
+        {
+            'url': '/static/1571.jpg',
+            'name': 'Imagen 2',
+            'link': '/images/2',
+        },
+    ]
+   
+    return render(request, 'image_detail.html', {'images': images})
+
